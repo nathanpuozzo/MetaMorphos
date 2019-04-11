359a7c577eba59d35040ee6b222fec0e9a733b74
nathanpuozzo <nathanpuozzo@gmail.com>
2019-04-11 09:31:47 +0200

    First publish
    Assets with sprites animation test and asset bundle test

---
+++ Assets/Scripts/AnimationHover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimationHover : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler {
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnPointerExit()
+    {
+        animator.SetBool("OnHover", false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+        animator.SetBool("OnHover", true);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //TODO : Load Assets Bundle in other Scene
+        Debug.Log("Clicked");
+    }
+}
